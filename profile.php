fed1c852daa6becdf729d5f96565dc8c5a342934
SpriteCT <weqaetfs@gmail.com>
2024-03-10 22:29:23 +0300

    added .gitignore, posts.php, edited profile.php

--- profile.php
+++ profile.php
@@ -68,9 +68,10 @@
                 </h1>
             </div>
             <div class="col-12">
-                <form method="POST" action="/profile.php">
+                <form method="POST" action="/profile.php" enctype="multipart/form-data" name="upload">
                     <div class="form_reg row"><input type ="text" class="form" name="title" placeholder="Заголовок вашего поста"></input></div>
                     <div class="form_reg row"><textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста ..."></textarea></div>
+                    <input type="file" name="file" /><br>
                     <div class="btn_add"><button type="submit" class="btn_red" name="submit">Сохранить пост!</button></add>
                 </form>
             </div>
@@ -96,5 +97,20 @@ if (isset($_POST['submit'])) {
         echo "Не удалось добавить пользователя";
     }
     mysqli_close($link);
+    if(!empty($_FILES["file"]))
+    {
+        if (((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
+        || (@$_FILES["file"]["type"] == "image/jpg") || (@$_FILES["file"]["type"] == "image/pjpeg")
+        || (@$_FILES["file"]["type"] == "image/x-png") || (@$_FILES["file"]["type"] == "image/png"))
+        && (@$_FILES["file"]["size"] < 102400))
+        {
+            move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $_FILES["file"]["name"]);
+            echo "Load in:  " . "uploads/" . $_FILES["file"]["name"];
+        }
+        else
+        {
+            echo "upload failed!";
+        }
+    }
 }
 ?>
